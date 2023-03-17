baa5359eeffbf117f9da2ffe8b31c4f837bb5162
derekvantilborg <dwvantilborg@gmail.com>
2023-03-17 16:20:07 +0100

    Data prep and master dataset class

--- active_learning/data_prep.py
+++ active_learning/data_prep.py
@@ -1,23 +1,15 @@
 
 
-
-# give all molecules an index
-# np array with ecfps
-# giant torch tensor with all graphs
-
-
+from active_learning.utils import molecular_graph_featurizer as smiles_to_graph
+from active_learning.utils import smiles_to_ecfp
 import pandas as pd
-import torch_geometric.data
-from tqdm import tqdm
-import torch
-from torch_geometric.loader import DataLoader
-from torch_geometric.data import Data
-from utils import molecular_graph_featurizer as smiles_to_graph
-from utils import smiles_to_ecfp
 import numpy as np
+import torch
+import os
+import sys
 from collections import OrderedDict
+from tqdm import tqdm
 from rdkit import Chem
-import random
 
 
 def canonicalize(smiles: str, sanitize: bool = True):
@@ -27,9 +19,9 @@ def canonicalize(smiles: str, sanitize: bool = True):
 def get_data(random_state: int = 42):
 
     # read smiles from file and canonicalize them
-    with open('data/inactives.smi') as f:
+    with open('data/original/inactives.smi') as f:
         inactives = [canonicalize(smi.strip().split()[0]) for smi in f.readlines()]
-    with open('data/actives.smi') as f:
+    with open('data/original/actives.smi') as f:
         actives = [canonicalize(smi.strip().split()[0]) for smi in f.readlines()]
 
     # remove duplicates:
@@ -51,43 +43,82 @@ def get_data(random_state: int = 42):
     return df
 
 
-def split_data(df: pd.DataFrame, random_state: int = 42, screen_test_split: float = 0.9):
+def split_data(df: pd.DataFrame, random_state: int = 42, screen_size: float = 0.9) -> (pd.DataFrame, pd.DataFrame):
 
     from sklearn.model_selection import train_test_split
-    df_screen, df_test = train_test_split(df, stratify=df['y'].tolist(), train_size=screen_test_split,
+    df_screen, df_test = train_test_split(df, stratify=df['y'].tolist(), train_size=screen_size,
                                           random_state=random_state)
 
     # write to csv
-    df_screen.to_csv('data/screen_0.csv', index=False)
-    df_test.to_csv('data/test_0.csv', index=False)
+    df_screen.to_csv('data/original/screen.csv', index=False)
+    df_test.to_csv('data/original/test.csv', index=False)
 
     return df_screen, df_test
 
 
-def featurize_data(df_screen, df_test):
+class dataset:
+    """ Dataset that holds all data in an indexable way """
+    def __init__(self, df: pd.DataFrame, name: str, representation: str = 'ecfp', root: str = 'data') -> None:
 
-    # Compute all molecular graphs and put them in a dict
-    screen_graphs = OrderedDict(((s, smiles_to_graph(s, y=y)) for s, y in tqdm(zip(df_screen.smiles, df_screen.y))))
-    test_graphs = OrderedDict(((s, smiles_to_graph(s, y=y)) for s, y in tqdm(zip(df_test.smiles, df_test.y))))
+        assert representation in ['ecfp', 'graph'], f"'representation' must be 'ecfp' or 'graph', not {representation}"
+        self.representation = representation
+        self.pth = os.path.join(root, name)
 
-    screen_ecfps = OrderedDict([('smiles_index', {smi: i for i, smi in enumerate(df_screen.smiles)}),
-                                ('smiles', np.array(df_screen.smiles.tolist())),
-                                ('x', smiles_to_ecfp(df_screen.smiles)),
-                                ('y', np.array(df_screen.y))])
+        # If not done already, process all data. Else just load it
+        if not os.path.exists(self.pth):
+            os.makedirs(os.path.join(root, name))
+            self.process(df)
+            self.smiles_index, self.index_smiles, self.smiles, self.x, self.y, self.graphs = self.load()
+        else:
+            self.smiles_index, self.index_smiles, self.smiles, self.x, self.y, self.graphs = self.load()
 
-    test_ecfps = OrderedDict([('smiles_index', {smi: i for i, smi in enumerate(df_test.smiles)}),
-                              ('smiles', np.array(df_test.smiles.tolist())),
-                              ('x', smiles_to_ecfp(df_test.smiles)),
-                              ('y', np.array(df_test.y))])
+    def process(self, df: pd.DataFrame) -> None:
 
-    torch.save(screen_graphs, 'data/screen_graphs.pt')
-    torch.save(test_graphs, 'data/test_graphs.pt')
-    torch.save(screen_ecfps, 'data/screen_ecfps.pt')
-    torch.save(test_ecfps, 'data/test_ecfps.pt')
+        print('Processing data ... ', flush=True, file=sys.stderr)
+
+        index_smiles = OrderedDict({i: smi for i, smi in enumerate(df.smiles)})
+        smiles_index = OrderedDict({smi: i for i, smi in enumerate(df.smiles)})
+        smiles = np.array(df.smiles)
+        x = smiles_to_ecfp(df.smiles, silent=False)
+        y = np.array(df.y)
+        graphs = [smiles_to_graph(smi, y=y) for smi, y in tqdm(zip(df.smiles, df.y))]
+
+        torch.save(index_smiles, os.path.join(self.pth, 'index_smiles'))
+        torch.save(smiles_index, os.path.join(self.pth, 'smiles_index'))
+        torch.save(smiles, os.path.join(self.pth, 'smiles'))
+        torch.save(x, os.path.join(self.pth, 'x'))
+        torch.save(y, os.path.join(self.pth, 'y'))
+        torch.save(graphs, os.path.join(self.pth, 'graphs'))
+
+    def load(self) -> (dict, dict, np.ndarray, np.ndarray, np.ndarray, list):
+
+        print('Loading data ... ', flush=True, file=sys.stderr)
+
+        index_smiles = torch.load(os.path.join(self.pth, 'index_smiles'))
+        smiles_index = torch.load(os.path.join(self.pth, 'smiles_index'))
+        smiles = torch.load(os.path.join(self.pth, 'smiles'))
+        x = torch.load(os.path.join(self.pth, 'x'))
+        y = torch.load(os.path.join(self.pth, 'y'))
+        graphs = torch.load(os.path.join(self.pth, 'graphs'))
+
+        return smiles_index, index_smiles, smiles, x, y, graphs
+
+    def __len__(self) -> int:
+        return len(self.smiles)
+
+    def __getitem__(self, idx):
+        if type(idx) is int:
+            idx = [idx]
+        if self.representation == 'ecfp':
+            return self.x[idx], self.y[idx]
+        if self.representation == 'graph':
+            return [self.graphs[i] for i in idx], self.y[idx]
 
 
 if __name__ == '__main__':
 
     df = get_data()
-    df_screen, df_test = split_data(df, screen_test_split=0.9)
-    featurize_data(df_screen, df_test)
+    df_screen, df_test = split_data(df, screen_size=0.9)
+
+    dataset(df_screen, 'screen')
+    dataset(df_test, 'test', 'graph')
