e2a33177caef99d50349aa633dba8a541a7161c3
derekvantilborg <dwvantilborg@gmail.com>
2023-03-20 15:19:10 +0100

    Eval now remembers cycles

--- active_learning/utils.py
+++ active_learning/utils.py
@@ -2,7 +2,9 @@
 from torch_geometric.data import Data
 from rdkit import Chem
 from tqdm import tqdm
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score, precision_score, recall_score, confusion_matrix
 import numpy as np
+import pandas as pd
 import torch
 
 
@@ -98,23 +100,6 @@ def atom_props(atom):
     return x
 
 
-# def molecule_from_smiles(smiles: str):
-#     """ Sanitize a molecule from a SMILES string"""
-#
-#     molecule = Chem.MolFromSmiles(smiles, sanitize=True)
-#     Chem.AssignStereochemistry(molecule, cleanIt=True, force=True)
-#
-#     # If sanitization is unsuccessful, catch the error, and try again without
-#     # the sanitization step that caused the error
-#     flag = Chem.SanitizeMol(molecule, catchErrors=True)
-#     if flag != Chem.SanitizeFlags.SANITIZE_NONE:
-#         Chem.SanitizeMol(molecule, sanitizeOps=Chem.SanitizeFlags.SANITIZE_ALL ^ flag)
-#
-#     # Chem.rdPartialCharges.ComputeGasteigerCharges(molecule)
-#
-#     return molecule
-
-
 def smiles_to_ecfp(smiles: list[str], radius: int = 2, nbits: int = 1024, silent: bool = True) -> np.ndarray:
     """ Get a Numpy array of ECFPs from a list of SMILES strings """
     from rdkit.Chem.AllChem import GetMorganFingerprintAsBitVect
@@ -136,97 +121,60 @@ def smiles_to_ecfp(smiles: list[str], radius: int = 2, nbits: int = 1024, silent
 
 
 class Evaluate:
-    def __init__(self, y_hat: torch.Tensor, y: torch.Tensor):
-        self.y_hat = y_hat.to('cpu')
-        self.y = y.to('cpu')
+    def __init__(self):
+        self.binary_accuracy = []
+        self.balanced_accuracy = []
+        self.roc_auc = []
+        self.precision = []
+        self.tpr = []
+        self.tn, self.fp, self.fn, self.tp = [], [], [], []
 
-        self.binary_accuracy = None
-        self.balanced_accuracy = None
-        self.roc_auc = None
-        self.precision = None
-        self.tpr = None
-        self.tn, self.fp, self.fn, self.tp = None, None, None, None
+    def eval(self, y_hat: torch.Tensor, y: torch.Tensor, threshold: float = 0.5):
 
-    def calc_binary_accuracy(self, threshold: float = 0.5):
-        y_hat = (self.y_hat > torch.Tensor([threshold])).float() * 1
+        y_hat = torch.tensor(y_hat) if type(y_hat) is np.ndarray else y_hat.cpu()
+        y = torch.tensor(y) if type(y) is np.ndarray else y.cpu()
 
-        acc = torch.sum(y_hat == self.y) / len(self.y)
+        y_hat_bin = (y_hat > torch.Tensor([threshold])).float() * 1
 
-        self.binary_accuracy = acc.item()
-        return self.binary_accuracy
+        # calc_binary_accuracy
+        acc = torch.sum(y_hat_bin == y) / len(y)
+        self.binary_accuracy.append(acc.item())
 
-    def calc_balanced_accuracy(self, threshold: float = 0.5):
-        from sklearn.metrics import balanced_accuracy_score
+        # calc_balanced_accuracy
+        balanced_acc = balanced_accuracy_score(y, y_hat_bin)
+        self.balanced_accuracy.append(balanced_acc)
 
-        y_hat = (self.y_hat > torch.Tensor([threshold])).float() * 1
-        balanced_acc = balanced_accuracy_score(self.y, y_hat)
+        # calc_roc_auc
+        self.roc_auc.append(roc_auc_score(y, y_hat))
 
-        self.balanced_accuracy = balanced_acc
-        return self.balanced_accuracy
+        # calc_precision
+        self.precision.append(precision_score(y, y_hat_bin))
 
-    def calc_roc_auc(self):
-        from sklearn.metrics import roc_auc_score
+        # calc recall
+        self.tpr.append(recall_score(y, y_hat_bin))
 
-        self.roc_auc = roc_auc_score(self.y, self.y_hat)
-        return self.roc_auc
-
-    def calc_precision(self, threshold: float = 0.5):
-        from sklearn.metrics import precision_score
-
-        y_hat = (self.y_hat > torch.Tensor([threshold])).float() * 1
-        self.precision = precision_score(self.y, y_hat)
-
-        return self.precision
-
-    def calc_recall(self, threshold: float = 0.5):
-        from sklearn.metrics import recall_score
-
-        y_hat = (self.y_hat > torch.Tensor([threshold])).float() * 1
-        self.tpr = recall_score(self.y, y_hat)
-
-        return self.tpr
-
-    def calc_confusion(self, threshold: float = 0.5):
-        from sklearn.metrics import confusion_matrix
-
-        y_hat = (self.y_hat > torch.Tensor([threshold])).float() * 1
-        self.tn, self.fp, self.fn, self.tp = confusion_matrix(self.y, y_hat).ravel()
-
-    def calc_hits(self, **kwargs):
-        if self.tp is None:
-            self.calc_confusion(**kwargs)
-        return self.tp
-
-    def calc_misses(self, **kwargs):
-        if self.fn is None:
-            self.calc_confusion(**kwargs)
-        return self.fn
-
-    def eval(self, silent: bool = False, threshold: float = 0.5):
-        self.calc_binary_accuracy(threshold=threshold)
-        self.calc_balanced_accuracy(threshold=threshold)
-        self.calc_precision(threshold=threshold)
-        self.calc_recall(threshold=threshold)
-        self.calc_roc_auc()
-        self.calc_hits(threshold=threshold)
-        self.calc_misses(threshold=threshold)
-
-        if not silent:
-            print(self)
+        # calc confusion
+        tn, fp, fn, tp = confusion_matrix(y, y_hat_bin).ravel()
+        self.tn.append(tn)
+        self.fp.append(fp)
+        self.fn.append(fn)
+        self.tp.append(tp)
 
     def __repr__(self):
-        return f"Binary accuracy:    {self.binary_accuracy:.4f}\n" \
-               f"Balanced accuracy:  {self.balanced_accuracy:.4f}\n" \
-               f"Precision:          {self.precision:.4f}\n" \
-               f"True positive rate: {self.tpr:.4f}\n" \
-               f"ROC AUC:            {self.roc_auc:.4f}\n" \
-               f"Hits:               {self.tp}\n" \
-               f"Misses:             {self.fn}\n" \
-               f"False positives:    {self.fp}\n" \
-               f"True negatives:     {self.tn}"
+        return f"Binary accuracy:    {self.binary_accuracy[-1]:.4f}\n" \
+               f"Balanced accuracy:  {self.balanced_accuracy[-1]:.4f}\n" \
+               f"Precision:          {self.precision[-1]:.4f}\n" \
+               f"True positive rate: {self.tpr[-1]:.4f}\n" \
+               f"ROC AUC:            {self.roc_auc[-1]:.4f}\n" \
+               f"Hits:               {self.tp[-1]}\n" \
+               f"Misses:             {self.fn[-1]}\n" \
+               f"False positives:    {self.fp[-1]}\n" \
+               f"True negatives:     {self.tn[-1]}\n"
 
-    def metrics_fstring(self):
-        if self.binary_accuracy is None:
-            self.eval(silent=True)
-        return f"{self.binary_accuracy},{self.balanced_accuracy},{self.precision},{self.tpr},{self.roc_auc}," \
-               f"{self.tp},{self.fn},{self.fp},{self.tn}"
+    def to_dataframe(self, colnames: str = ''):
+        df = pd.DataFrame({'binary_accuracy': self.binary_accuracy, 'balanced_accuracy': self.balanced_accuracy,
+                           'precision': self.precision, 'tpr': self.tpr, 'roc_auc': self.roc_auc,
+                           'tp': self.tp, 'fn': self.fn, 'fp': self.fp, 'tn': self.tn})
+        df.columns = [f"{colnames}{i}" for i in df.columns]
+
+        return df
