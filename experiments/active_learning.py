2f6bb2ef608fa841ec658ecca77967dc50909b87
derekvantilborg <dwvantilborg@gmail.com>
2023-05-09 08:50:03 +0200

    master script calling the active learning loop

---
+++ experiments/active_learning.py
@@ -0,0 +1,57 @@
+
+import os
+from tqdm import tqdm
+from active_learning.screening import active_learning
+import itertools
+import argparse
+
+from config import ROOT_DIR
+
+PARAMETERS = {'max_screen_size': [1000],
+              'n_start': [64],
+              'batch_size': [16, 32, 64],
+              'architecture': ['gcn', 'mlp'],
+              'seed': list(range(20)),
+              'bias': ['random', 'small', 'large'],
+              'acquisition': ['random', 'exploration', 'exploitation', 'dynamic', 'batch_bald', 'similarity']
+              }
+
+
+if __name__ == '__main__':
+
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-o', help='The path of the output directory', default='results')
+    # parser.add_argument('-acq', help="Acquisition function ('random', 'exploration', 'exploitation', 'dynamic', "
+    #                                  "'batch_bald', 'similarity')", default='random')
+    # parser.add_argument('-bias', help='The level of bias ("random", "small", "large")', default='results')
+    # parser.add_argument('-arch', help='The neural network architecture ("gcn", "mlp")', default='mlp')
+    # args = parser.parse_args()
+    #
+    # PARAMETERS['acquisition'] = [args.acq]
+    # PARAMETERS['bias'] = [args.bias]
+    # PARAMETERS['architecture'] = [args.arch]
+    # LOG_FILE = f'{args.o}/{args.acq}){args.bias}){args.arch})simulation_results.csv'
+
+    LOG_FILE = f'simulation_results.csv'
+
+    experiments = [dict(zip(PARAMETERS.keys(), v)) for v in itertools.product(*PARAMETERS.values())]
+
+    for experiment in tqdm(experiments):
+
+        results = active_learning(n_start=experiment['n_start'],
+                                  bias=experiment['bias'],
+                                  acquisition_method=experiment['acquisition'],
+                                  max_screen_size=experiment['max_screen_size'],
+                                  batch_size=experiment['batch_size'],
+                                  architecture=experiment['architecture'],
+                                  seed=experiment['seed'],
+                                  optimize_hyperparameters=False)
+
+        # Add the experimental settings to the outfile
+        results['acquisition_method'] = experiment['acquisition']
+        results['architecture'] = experiment['architecture']
+        results['n_start'] = experiment['n_start']
+        results['batch_size'] = experiment['batch_size']
+        results['replica'] = experiment['replica']
+
+        results.to_csv(LOG_FILE, mode='a', index=False, header=False if os.path.isfile(LOG_FILE) else True)
