731642eb09f892079ec01fbed3e5923aec0b9d6c
derekvantilborg <dwvantilborg@gmail.com>
2023-05-12 16:16:35 +0200

    changed the hyperparameter search space

--- active_learning/hyperparameters.py
+++ active_learning/hyperparameters.py
@@ -1,14 +1,15 @@
 """ Range of hyperparameters that will be optimized for each model"""
 
-MLP_hypers = {'lr': [1e-3, 1e-4, 1e-5],                 # categorical
-              'n_hidden': [1024],                       # categorical
-              'epochs': [50, 100, 250, 500],
-              'ensemble_size': [10]}                    # categorical
+MLP_hypers = {'lr': [3e-3, 3e-4, 3e-5],                 # categorical
+              'n_hidden': [512, 1024],                  # categorical
+              'epochs': [100],
+              'anchored': [True],
+              'ensemble_size': [5]}                     # categorical
 
 
-GCN_hypers = {'lr': [1e-3, 1e-4, 1e-5],                 # categorical
-              'n_hidden': [1024],                       # categorical
-              'epochs': [50, 100, 250],                 # categorical
-              'num_conv_layers': [3, 5],
-              'ensemble_size': [10]}                    # integer range from 2 - 5
-
+GCN_hypers = {'lr': [3e-3, 3e-4, 3e-5],                 # categorical
+              'n_hidden': [512, 1024],                  # categorical
+              'epochs': [100],                          # categorical
+              'anchored': [True],
+              'num_conv_layers': [3],
+              'ensemble_size': [5]}                     # integer range from 2 - 5
