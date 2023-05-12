b1d56744958a467525fb533a70dd5a181981d772
derekvantilborg <dwvantilborg@gmail.com>
2023-05-12 16:19:48 +0200

    bash script for clustering on Snellius

--- jobs/clustering.sh
+++ jobs/clustering.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=clustering
-#SBATCH --output=/home/tilborgd/projects/random/out/clustering.out
+#SBATCH --output=/home/tilborgd/projects/Active_Learning_Simulation/out/clustering.out
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=128
@@ -8,5 +8,5 @@
 #SBATCH --time=01:00:00
 
 source $HOME/anaconda3/etc/profile.d/conda.sh
-export PYTHONPATH="${PYTHONPATH}:$HOME/projects/random"
-$HOME/anaconda3/envs/molml/bin/python -u $HOME/projects/random/snellius_cluster.py -o /home/tilborgd/projects/random > $HOME/projects/random/clustering.log
+export PYTHONPATH="${PYTHONPATH}:$HOME/projects/Active_Learning_Simulation"
+$HOME/anaconda3/envs/molml/bin/python -u $HOME/projects/Active_Learning_Simulation/experiments/snellius_cluster.py -o /home/tilborgd/projects/Active_Learning_Simulation > $HOME/projects/Active_Learning_Simulation/results/clustering.log
